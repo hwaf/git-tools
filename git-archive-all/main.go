801060ab404709da2385550f1d1d9681160f8f4a
Sebastien Binet <binet@cern.ch>
2013-01-18 14:11:16 +0100

    archive-all: dummy impl

---
+++ git-archive-all/main.go
@@ -0,0 +1,36 @@
+package main
+
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"os"
+// 	"os/exec"
+// 	"path/filepath"
+// 	"strings"
+
+// 	"github.com/mana-fwk/git-tools/utils"
+// )
+
+// var (
+// 	g_no_commit = flag.Bool("no-commit", false, "do not commit the result")
+// 	g_verbose   = flag.Bool("verbose", false, "")
+// )
+
+// func debug(cmd *exec.Cmd) {
+// 	if *g_verbose {
+// 		dir := cmd.Dir
+// 		if dir == "" {
+// 			dir, _ = os.Getwd()
+// 		}
+// 		fmt.Printf(">>> %v (%s)\n", cmd.Args, dir)
+// 		cmd.Stdin = os.Stdin
+// 		cmd.Stdout = os.Stdout
+// 		cmd.Stderr = os.Stderr
+// 	}
+// }
+
+func main() {
+
+}
+
+// EOF
